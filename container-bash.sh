52ba9a94f0d86c746e5a52a7786238d44dba058e
Nick Fox-Gieg <nick@fox-gieg.com>
2020-09-22 13:41:42 -0400

    scripts

--- container-bash.sh
+++ container-bash.sh
@@ -1,1 +1,2 @@
-docker exec -it $1 $2
+docker exec -it $1 bash
+
--- container-python-setup.sh
+++ container-python-setup.sh
@@ -2,9 +2,9 @@
 # https://linuxize.com/post/how-to-install-pip-on-ubuntu-18.04/
 # https://unix.stackexchange.com/questions/410579/change-the-python3-default-version-in-ubuntu
 
-SESSION_NAME=$1
+CONTAINER_NAME=$1
 PYTHON_VERSION=$2
-CMD="docker exec -it $SESSION_NAME"
+CMD="docker exec -it $CONTAINER_NAME"
 
 $CMD apt-get install -y software-properties-common
 $CMD add-apt-repository ppa:deadsnakes/ppa
