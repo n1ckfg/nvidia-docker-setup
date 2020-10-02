300e00e7dc0322c092d564e6d486c152a8e9e0ef
n1ckfg <nick@fox-gieg.com>
2020-10-02 19:06:03 -0400

    shell scripts

---
+++ cuda/nvidia-drivers.sh
@@ -0,0 +1,6 @@
+# https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-18-04-bionic-beaver-linux
+
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo ubuntu-drivers autoinstall
+
