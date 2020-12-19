c49b5521a60139ba628575862e8be7328ea48467
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-19 13:56:08 -0500

    shell scripts

--- list.sh
+++ list.sh
@@ -1,2 +1,10 @@
+echo ""
+echo "* * *   RUNNING CONTAINERS   * * *"
+docker ps
+echo ""
+echo "* * *   ALL CONTAINERS   * * *"
 docker container ls -a
+echo ""
+echo "* * *   DISK IMAGES   * * *"
+docker images
 
