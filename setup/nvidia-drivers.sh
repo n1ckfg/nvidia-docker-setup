ffdc2aeaf542513ba281c02198ba3f64ea3cfaa8
Nick Fox-Gieg <nick@fox-gieg.com>
2021-07-29 23:04:08 -0400

    shell

--- setup/nvidia-drivers.sh
+++ setup/nvidia-drivers.sh
@@ -1,5 +1,12 @@
 # https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-18-04-bionic-beaver-linux
 
+read -p "Warning: never run this on WSL. Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo ubuntu-drivers autoinstall
