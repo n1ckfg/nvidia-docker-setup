427cfd9d0bf34211a2913784b2cbc06f0964a3da
Nick Fox-Gieg <nick@fox-gieg.com>
2021-08-19 13:24:44 -0600

    wsl script

--- setup/wsl/nvidia-wsl2-setup.sh
+++ setup/wsl/nvidia-wsl2-setup.sh
@@ -1,5 +1,6 @@
 # https://docs.nvidia.com/cuda/wsl-user-guide/index.html
 
+# Follow the instructions below to install the CUDA Toolkit from the WSL-Ubuntu package on Ubuntu.
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
@@ -7,3 +8,16 @@ sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
+
+# Now install the NVIDIA Container Toolkit.
+#curl https://get.docker.com | sh
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container-experimental.list | sudo tee /etc/apt/sources.list.d/libnvidia-container-experimental.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
+# Open a separate WSL 2 window and start the Docker daemon again using the following commands to complete the installation.
+sudo service docker stop
+sudo service docker start
